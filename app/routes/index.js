a407e6cef80150292310b45d8f5cba322223f28d
Diego Meléndez <diedu89@gmail.com>
2017-11-11 15:59:34 -0600

    sequelize configuration
    Added sequelize, sequelize-cli and pg packages
    Migrations scaffold generated

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var { sequelize } = require('../models')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	sequelize
+	  .authenticate()
+	  .then(() => {
+	    console.log('Connection has been established successfully.');
+	  })
+	  .catch(err => {
+	    console.error('Unable to connect to the database:', err);
+	  });
   res.render('index', { title: 'Express' });
 });
 
