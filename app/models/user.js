5fb82a9d51a5f7259ac86127288c80fa012ee397
Diego Meléndez <diedu89@gmail.com>
2017-11-11 18:43:11 -0600

    Added user model

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
