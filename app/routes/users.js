9777d9815bfb4f691e4bf173f0afabfc5547c8ec
Diego Meléndez <diedu89@gmail.com>
2017-11-12 15:29:59 -0600

    Added test with database access

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var { User } = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.findAndCountAll({offset: 0, limit: 10})
+  	.then(function(result){
+  		res.send(result);
+  	})
 });
 
 module.exports = router;
