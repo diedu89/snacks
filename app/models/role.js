b1cd6ffbfcf3a2daad71ff37f58574ea0cea9667
Diego Meléndez <diedu89@gmail.com>
2017-11-12 01:47:04 -0600

    Some fixes in models
    Also added hashing when saving an user and faker package to seed data

--- app/models/role.js
+++ app/models/role.js
@@ -2,17 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   var Role = sequelize.define('Role', {
     role: { type: DataTypes.STRING, unique: true }
-  }, {
-    classMethods: {
-      associate: function(models) {
-        this.hasMany(models.User, {
-          onDelete: "CASCADE",
-          foreignKey: {
-            allowNull: false
-          }
-        })
-      }
-    }
   });
+
+  Role.associate = function(models) {
+    this.hasMany(models.User);
+  };
+
   return Role;
 };
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,29 @@
 'use strict';
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
-    email: { type: DataTypes.STRING, unique:true, validate:{ isEmail: true } },
-    username: { type: DataTypes.STRING, unique:true },
+    email: {type: DataTypes.STRING, unique:true, validate: {isEmail: true}},
+    username: {type: DataTypes.STRING, unique:true},
     password: DataTypes.STRING
   }, {
-    classMethods: {
-      associate: function(models) {
-        this.belongsTo(models.Role, {
-          onDelete: "CASCADE",
-          foreignKey: {
-            allowNull: false
-          }
-        });
-      },
-      testMethod: function(){
-        console.log("a model test method, here business logic will be store")
+    hooks:{
+      beforeCreate: function(user, options){
+        user.password = bcrypt.hashSync(user.password);
       }
     }
   });
+
+  User.associate = function(models) {
+    User.belongsTo(models.Role, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  }
+
   return User;
 };
