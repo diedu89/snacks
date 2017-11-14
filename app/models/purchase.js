0ecde3c35ff5905c7e6348086089a1b705aca6a9
Diego Meléndez <diedu89@gmail.com>
2017-11-14 00:48:09 -0600

    Added purchase model

---
+++ app/models/purchase.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Purchase = sequelize.define('Purchase', {
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    currentPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }
+  }, {
+    hooks: {
+      afterBulkCreate: function(purchases, options){
+        console.log("-------------- hook after create ----------");
+      }
+    }
+  });
+
+  Purchase.associate = function(models) {
+    this.belongsTo(models.User, {onDelete: "CASCADE"});
+    this.belongsTo(models.Product, {onDelete: "CASCADE"});
+  };
+
+  return Purchase;
+};
